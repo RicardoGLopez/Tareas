872255931641704c053102d8c93ec25f8ed10606
Ricardo López <ricardo.lopezfranco@galileo.edu>
2022-02-07 23:00:24 -0600

    Semana2

---
+++ Actividad#2 Ricardo Gabriel López Franco.js
@@ -0,0 +1,44 @@
+var mes = 6
+var NoC= "No.Carne: 22004254"
+var Nombre="Nombre: Ricardo Gabriel López Franco"
+
+switch(mes){
+  case 1:
+    console.log("Es Invierno,"+" "+ NoC +", "+ Nombre)
+    break;
+  case 2:
+    console.log("Es Invierno,"+" "+ NoC +", "+ Nombre)
+    break;
+  case 3:
+    console.log("Es Primavera,"+" "+ NoC +", "+ Nombre)
+    break;
+  case 4:
+    console.log("Es Primavera,"+" "+ NoC +", "+ Nombre)
+    break;
+  case 5:
+    console.log("Es Primavera,"+" "+ NoC +", "+ Nombre)
+    break;
+  case 6:
+    console.log("Es Verano,"+" "+ NoC +", "+ Nombre)
+    break;
+  case 7:
+    console.log("Es Verano,"+" "+ NoC +", "+ Nombre)
+    break;
+  case 8:
+    console.log("Es Verano,"+" "+ NoC +", "+ Nombre)
+    break;
+  case 9:
+    console.log("Es Otoño,"+" "+ NoC +", "+ Nombre)
+    break;
+  case 10:
+    console.log("Es Otoño,"+" "+ NoC +", "+ Nombre)
+    break;
+  case 11:
+    console.log("Es Otoño,"+" "+ NoC +", "+ Nombre)
+    break;
+  case 12:
+    console.log("Es Invierno,"+" "+ NoC +", "+ Nombre)
+    break;
+    
+
+}
